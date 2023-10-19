ad8e2b69d01363fa908775da7f76c660f540b3e2
Daniyal Farhan <143825888+Daniyalfarhan15@users.noreply.github.com>
2023-10-19 18:46:25 +0500

    Add files via upload

---
+++ counter js/apps.js
@@ -0,0 +1,21 @@
+const number = document.querySelector('#number')
+// const buttons = document.querySelectorAll('button')
+
+console.log(number)
+// console.log(buttons)
+
+function increment(){
+    console.log("increment", number.textContent)
+    number.textContent = +(number.textContent) + 1
+}
+function decrement() {
+    // if(number.textContent == 0)
+    console.log("decrement", number.textContent)
+    number.textContent = number.textContent - 1
+}
+function reset() {
+    console.log("reset", number.textContent)
+    number.textContent = 0
+}
+
+
